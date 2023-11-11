27adfe05a6a9918b4ca11d40b5219d7d932b27c5
NikitaRyzhkov <ryzhkov134@gmail.com>
2023-11-12 00:17:24 +0300

    Добавлены тесты левого бокового меню

--- src/test/java/LeftMenuTest.java
+++ src/test/java/LeftMenuTest.java
@@ -1,9 +1,14 @@
 import elements.CalendarElement;
 import elements.LeftMenuElement;
+import org.testng.Assert;
 import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
 import pages.EventPage;
+import pages.settings.TimeZonePage;
 
-import static com.codeborne.selenide.Condition.hidden;
+import java.util.List;
+
+import static com.codeborne.selenide.Condition.enabled;
 import static com.codeborne.selenide.Condition.visible;
 
 public class LeftMenuTest extends SignIn {
@@ -13,6 +18,7 @@ public class LeftMenuTest extends SignIn {
         LeftMenuElement leftMenuElement = new LeftMenuElement();
         leftMenuElement.clickUserCalendarBtn();
         leftMenuElement.popupCalendar.shouldBe(visible);
+        leftMenuElement.clickUserCalendarBtn();
     }
     @Test
     public void createEvent(){
@@ -21,9 +27,67 @@ public class LeftMenuTest extends SignIn {
         CalendarElement calendarElement = new CalendarElement();
 
         menuElement.clickEventBtn();
-        eventPage.inputName("abcdef1");
+        eventPage.inputName("Test event");
         eventPage.clickSaveBtn();
         calendarElement.randomEvent.shouldBe(visible);
+    }
+    @Test
+    public void calendarActionsMenu(){
+        LeftMenuElement menuElement = new LeftMenuElement();
+        menuElement.clickCalendarActionsBtn();
+        menuElement.calendarDropDownMenu.shouldBe(visible);
+        menuElement.appsList.click();
+    }
+    @Test
+    public void calendarSettingsMenu(){
+        LeftMenuElement menuElement = new LeftMenuElement();
+        menuElement.clickCalendarSettingsBtn();
+        menuElement.calendarDropDownMenu.shouldBe(visible);
+        menuElement.shareOption.shouldBe(visible);
+        menuElement.appsList.click();
+    }
+    @Test
+    public void hideNShowMiniCalendar(){
+        LeftMenuElement menuElement = new LeftMenuElement();
+
+        menuElement.clickHideMiniCalendarBtn();
+        menuElement.miniCalendar.shouldNotBe(visible);
+
+        menuElement.clickHideMiniCalendarBtn();
+        menuElement.miniCalendar.shouldBe(visible);
+    }
+    @Test
+    public void changeMonth(){
+        LeftMenuElement menuElement = new LeftMenuElement();
+        String currentMonth = menuElement.monthName.getText();
+        String currentSlot = menuElement.firstSlotMiniCalendar.getText();
+        menuElement.clickNextMonthBtn();
+
+        SoftAssert softAssert = new SoftAssert();
+        softAssert.assertNotEquals(menuElement.monthName.getText(),currentMonth);
+        softAssert.assertNotEquals(menuElement.firstSlotMiniCalendar.getText(),currentSlot);
+        softAssert.assertAll();
+    }
+    @Test
+    public void miniCalendarSlot(){
+        LeftMenuElement menuElement = new LeftMenuElement();
+        CalendarElement calendarElement = new CalendarElement();
+        String slot = "15";
+        menuElement.clickMiniCalendarSlot(slot);
+
+        List<String> days = calendarElement.weekDays.texts();
+        Assert.assertTrue(days.contains(slot));
+
+    }
+    @Test
+    public void timeZone(){
+        LeftMenuElement menuElement = new LeftMenuElement();
+        TimeZonePage timeZonePage = new TimeZonePage();
+
+        menuElement.clickTimeZoneBtn();
+        timeZonePage.header.shouldBe(visible);
+        timeZonePage.changeZoneBtn.shouldBe(enabled);
+        timeZonePage.closeDialog();
     }
 
 }
