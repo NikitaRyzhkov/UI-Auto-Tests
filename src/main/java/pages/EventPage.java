1b144fa62a4bf44093a1455cf6680ef7a672aada
NikitaRyzhkov <ryzhkov134@gmail.com>
2023-11-12 15:39:21 +0300

    Добавлены аннотации Allure @Description

--- src/main/java/pages/EventPage.java
+++ src/main/java/pages/EventPage.java
@@ -1,7 +1,6 @@
 package pages;
 
 import com.codeborne.selenide.SelenideElement;
-import com.codeborne.selenide.selector.ByText;
 import io.qameta.allure.Step;
 
 import static com.codeborne.selenide.Selenide.$;
